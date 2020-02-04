76d453b5fa1b550fe08235a31d25a892ac62cc90
Christhopher Porpino <cgporpinof@gmail.com>
2020-02-04 10:18:02 -0300

    ajustei cor do tema e largura do ActionButton

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,8 +1,19 @@
 import { createMuiTheme } from "@material-ui/core";
-import orange from "@material-ui/core/colors/orange";
 
 export default createMuiTheme({
   palette: {
-    primary: orange
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#5cb646',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    // secondary: {
+    //   light: '#0066ff',
+    //   main: '#0044ff',
+    //   dark: will be calculated from palette.secondary.main,
+    //   contrastText: '#ffcc00',
+    // },
+    // error: will use the default color
   }
 });
