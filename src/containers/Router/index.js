85c317cd5e5f8674497a1355228a8aa0282774b0
MateusGesualdo <mateusgcs99@gmail.com>
2020-02-04 09:21:35 -0300

    rotas criadas

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -5,15 +5,25 @@ import InitialPage from '../Initial/index'
 import Login from '../Login/index'
 import SignUp from '../SignUp/index'
 import Address from '../Address/index'
+import Home from '../Home/index'
+import SelectedRestaurant from '../SelectedRestaurant/index'
 import Cart from '../Cart/index'
+import Profile from '../Profile/index'
+import EditProfile from '../EditProfile/index'
+import EditAddress from '../EditAddress/index'
 
 
 export const routes = {
-  root: '/',
+  root: '/',  
   login: '/login/',
   signUp: '/signup/',
   address: '/address/',
-  cart: '/cart/'
+  home: '/home/' ,
+  restaurants: '/restaurants/',
+  cart: '/cart/',
+  profile: '/profile/',
+  editAddress: '/address/edit',
+  editProfile: '/profile/edit/',  
 };
 
 function Router(props) {
@@ -24,7 +34,12 @@ function Router(props) {
         <Route exact path={routes.login} component={Login} />
         <Route exact path={routes.signUp} component={SignUp} />
         <Route exact path={routes.address} component={Address} />
+        <Route exact path={routes.home} component={Home} />
+        <Route exact path={routes.restaurants} component={SelectedRestaurant} />
         <Route exact path={routes.cart} component={Cart} />
+        <Route exact path={routes.profile} component={Profile} />
+        <Route exact path={routes.editProfile} component={EditProfile} />
+        <Route exact path={routes.editAddress} component={EditAddress} />        
       </Switch>
     </ConnectedRouter>
   );
