f4ec87845d5fb74a732879aab8ea8b8694460698
Christhopher Porpino <cgporpinof@gmail.com>
2020-02-05 17:51:28 -0300

    criei barra de pesquisa

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    search: {
+        position: 'relative',
+        width: '100%',
+        border: '1px solid #b8b8b8'
+    },
+    searchIcon: {
+        width: theme.spacing.unit * 7,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 8,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
+});
+
+class SearchBar extends React.Component {
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <AppBar color="" position="static">
+                    <Toolbar>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="Restaurante"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(SearchBar);
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import BottomNav from "../../components/BottomNav";
 import CategoryTabs from '../../components/CategoryTabs';
+import { HeaderNav } from '../../components/HeaderNav';
+import SearchBar from '../../components/SearchBar';
 
-function Home(){
+function Home(props){
     //react hooks
 
     return (
         <div>
+            <HeaderNav onClick={() => props.goBack() } menuText="FutureEats" />
+            <SearchBar />
             <CategoryTabs />
             <BottomNav />
         </div>
