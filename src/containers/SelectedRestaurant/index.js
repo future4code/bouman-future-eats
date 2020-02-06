689b39f0193e46b5f635b781550444975ef5436e
MateusGesualdo <mateusgcs99@gmail.com>
2020-02-06 15:47:05 -0300

    Integração até o SelectQuantity

--- src/containers/SelectedRestaurant/index.js
+++ src/containers/SelectedRestaurant/index.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import SelectQuantity from '../../components/SelectQuantity'
+import { RestaurantCard } from '../../components/RestaurantCard'
+import { DishCard } from '../../components/DishCard'
+import { HeaderNav } from '../../components/HeaderNav'
+import { connect } from 'react-redux'
+import { goBack } from 'connected-react-router'
 
 
 export function SelectedRestaurants(props) {
     return (
         <div>
-        <SelectQuantity>
-        </SelectQuantity>
+            <HeaderNav menuText='Restaurante' onClick={props.goBack}/>
+            <RestaurantCard
+                name={props.selectedRestaurant.name}
+                photoUrl={props.selectedRestaurant.logoUrl}
+                category={props.selectedRestaurant.category}
+                address={props.selectedRestaurant.address}
+                shipping={props.selectedRestaurant.shipping}
+                deliveryTime={props.selectedRestaurant.deliveryTime}
+            />
+            <h3>Refeições</h3>
+            <hr />
+            {props.selectedRestaurant.products.map(
+                dish => <DishCard
+                    name={dish.name}
+                    photoUrl={dish.photoUrl}
+                    price={dish.price}
+                    description={dish.description}
+                    
+                />
+            )}
+            {/* <SelectQuantity /> */}
+
 
         </div>
     )
 }
 
 
-export default SelectedRestaurants;
+export default connect(
+    state => ({
+        selectedRestaurant: state.restaurants.selectedRestaurant
+    }),
+    dispatch => ({
+        goBack: () => dispatch(goBack())
+    })
+)(SelectedRestaurants);
 
