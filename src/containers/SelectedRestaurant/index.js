38fbfa225551fb7cf79467bf21fd10a52e62904b
gui-abq <guilhermeabq@gmail.com>
2020-02-07 02:15:54 -0300

    terminando a pagina de cart

--- src/containers/SelectedRestaurant/index.js
+++ src/containers/SelectedRestaurant/index.js
@@ -7,34 +7,66 @@ import { HeaderNav } from '../../components/HeaderNav'
 import { connect } from 'react-redux'
 import { goBack } from 'connected-react-router'
 
+const MainContainer = styled.div `
+display: flex;
+width: 95%;
+flex-direction: column;
+align-items: center;
+margin-top: 10px;
+`
+
+const StyledDishMenuTittle = styled.p `
+  width: 328px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #000000;
+  border-bottom: 1px solid black;
+  padding-bottom: 5px;
+
+
+`
+
+const AjustScreenMobile = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+
 
 export function SelectedRestaurants(props) {
     return (
-        <div>
+        <AjustScreenMobile>
             <HeaderNav menuText='Restaurante' onClick={props.goBack}/>
-            <RestaurantCard
-                name={props.selectedRestaurant.name}
-                photoUrl={props.selectedRestaurant.logoUrl}
-                category={props.selectedRestaurant.category}
-                address={props.selectedRestaurant.address}
-                shipping={props.selectedRestaurant.shipping}
-                deliveryTime={props.selectedRestaurant.deliveryTime}
-            />
-            <h3>Refeições</h3>
-            <hr />
-            {props.selectedRestaurant.products.map(
-                dish => <DishCard
-                    name={dish.name}
-                    photoUrl={dish.photoUrl}
-                    price={dish.price}
-                    description={dish.description}
-                    
+            <MainContainer>
+                <RestaurantCard
+                    name={props.selectedRestaurant.name}
+                    photoUrl={props.selectedRestaurant.logoUrl}
+                    category={props.selectedRestaurant.category}
+                    address={props.selectedRestaurant.address}
+                    shipping={props.selectedRestaurant.shipping}
+                    deliveryTime={props.selectedRestaurant.deliveryTime}
                 />
-            )}
+                <StyledDishMenuTittle>Refeições</StyledDishMenuTittle>
+                    <hr/>
+                {props.selectedRestaurant.products.map(
+                    dish => <DishCard
+                        name={dish.name}
+                        photoUrl={dish.photoUrl}
+                        price={dish.price}
+                        description={dish.description}                       
+                    />
+
+                    )}
+                </MainContainer>
             {/* <SelectQuantity /> */}
 
 
-        </div>
+        </AjustScreenMobile>
     )
 }
 
