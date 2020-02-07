7066d2f3f8f52e97f1ef4ccea93e994c0cc1932f
Christhopher Porpino <cgporpinof@gmail.com>
2020-02-07 13:00:24 -0300

    implementando renderização condicional

--- src/containers/SearchPage/index.js
+++ src/containers/SearchPage/index.js
@@ -14,49 +14,61 @@ const Text = styled.div`
   align-items: center;
   margin-top: 20px;
 `
-
 function SearchPage(props) {
     //react hooks
     const [restaurantName, setName] = React.useState("")
 
-    return (
-        <div>
-            <HeaderNav onClick={() => props.goBack()} menuText="Busca" />
-            <SearchBar
-                value={restaurantName}
-                onChange={(ev) => (setName(ev.target.value))}
-            />
-            <Text>
-                <Typography variant="h6">
-                    Busque por nome de restaurante
-                </Typography>
-            </Text>
-
+    if (restaurantName) {
+        return (
             <div>
+                <HeaderNav onClick={() => props.goBack()} menuText="Busca" />
+                <SearchBar
+                    value={restaurantName}
+                    onChange={(ev) => (setName(ev.target.value))}
+                />
+                <Text>
+                    <Typography variant="h6">
+                        Busque por nome de restaurante
+                        </Typography>
+                </Text>
 
-                {props.restaurantList.filter((restaurant) => restaurant.name.includes(restaurantName))
-                    .map(
-                        (restaurant) => <RestaurantCard
-                            name={restaurant.name}
-                            category={restaurant.category}
-                            photoUrl={restaurant.logoUrl}
-                            shipping={restaurant.shipping}
-                            deliveryTime={restaurant.deliveryTime}
-                            onclick={() => props.getRestaurantDetails(restaurant.id)}
-
-                        />
-                    )}
+                <div>
+                    {props.restaurantList.filter((restaurant) => restaurant.name.includes(restaurantName))
+                        .map(
+                            (restaurant) => <RestaurantCard
+                                name={restaurant.name}
+                                category={restaurant.category}
+                                photoUrl={restaurant.logoUrl}
+                                shipping={restaurant.shipping}
+                                deliveryTime={restaurant.deliveryTime}
+                                onclick={() => props.getRestaurantDetails(restaurant.id)}
+                            />
+                        )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    } if (setName) {
+        return (
+            <div>
+                <HeaderNav onClick={() => props.goBack()} menuText="Busca" />
+                <SearchBar
+                    value={restaurantName}
+                    onChange={(ev) => (setName(ev.target.value))}
+                />
+                <Text>
+                    <Typography variant="h6">
+                        Busque por nome de restaurante
+                        </Typography>
+                </Text>
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state) {
     return ({
         restaurantList: state.restaurants.restaurantList,
-
     })
-
 }
 
 const mapDispatchToProps = dispatch => ({
